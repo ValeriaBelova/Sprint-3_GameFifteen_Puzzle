fa01a716611c4b41a8a6bfe899b9b4ae2b36696e
ValeriaBelova <114664655+ValeriaBelova@users.noreply.github.com>
2022-11-03 14:19:23 +0100

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,10 @@
+package Sprint3.InlämningsUppgift3Game15;
+
+import javax.swing.*;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        JFrame frame = new Game();
+        frame.setVisible(true);
+    }
+}
